a39b17162a359b5eb8606ac6cd53507440d56e85
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2025-01-24 09:13:47 +0500

    realize domain entity and repository interface

--- internal/domain/repository/product.go
+++ internal/domain/repository/product.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"github.com/Jasurbek-Tursunov/warehouse/internal/domain/entity"
+	"github.com/Jasurbek-Tursunov/warehouse/internal/domain/repository/dto"
+)
+
+type ProductRepository interface {
+	List() ([]entity.Product, error)
+	Create(product dto.CreateProduct) (entity.Product, error)
+	Update(product dto.UpdateProduct) (entity.Product, error)
+	Get(id int) (entity.Product, error)
+	Delete(id string) error
+}
--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -3,6 +3,6 @@ package repository
 import "github.com/Jasurbek-Tursunov/warehouse/internal/domain/entity"
 
 type UserRepository interface {
-	Create(user *entity.User) error
+	Create(user *entity.User) (*entity.User, error)
 	GetByUsername(username string) (*entity.User, error)
 }
